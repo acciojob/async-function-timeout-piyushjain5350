fb0da13c54c51fd6865c7e3920f05f2d21fb9fc2
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-06-28 20:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const text=document.getElementById("text");
+const delayedtimer=document.getElementById("delay");
+const button=document.getElementById("btn");
+
+
+button.addEventListener("onclick", click());
+
+function click(){
+	console.log("inside a");
+}
